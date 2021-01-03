b9269cb9a1d7cfa5174fa87ba910cf1a057452d8
Donghwan Seo <tonky0110@gmail.com>
2021-01-03 15:17:41 +0900

    1.1 add Redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
+
+const reducer = () => {};
+
+const store = createStore(reducer);
 
 let count = 0;
 
