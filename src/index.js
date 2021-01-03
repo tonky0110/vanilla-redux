0effce5b194d334276697af10fe21761a96eefa2
Donghwan Seo <tonky0110@gmail.com>
2021-01-03 16:54:17 +0900

    2.1 todo list with redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
+import { createStore } from 'redux';
+
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
 
-const createToDo = toDo => {
-  const li = document.createElement('li');
-  li.innerText = toDo;
-  ul.append(li);
+// redux
+// action
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+const reducer = (state = [], action = {}) => {
+  console.log(action);
+  switch(action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
 }
+
+const store = createStore(reducer);
+
 
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
-  createToDo(toDo);
+  input.value = '';
+  store.dispatch({ type: ADD_TODO, text: toDo });
 }
 
 
 form.addEventListener('submit', onSubmit);
+ 
