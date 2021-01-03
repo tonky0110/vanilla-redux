3f5a81f5a5d685a47856f34c518910a34d80d7ee
Donghwan Seo <tonky0110@gmail.com>
2021-01-03 16:14:45 +0900

    개선1. if - else if --> switch문 사용

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ number.innerText = 0;
 
 const countModifier = (count = 0, action) => {
   console.log(count, action);
-  if (action.type === 'ADD') {
-    return count + 1;
-  } else if (action.type === 'MINUS') {
-    return count - 1;
-  } else {
-    return count;
+  switch(action.type) {
+    case "ADD":
+      return count + 1;
+    case "MINUS":
+      return count - 1;
+    default:
+      return count;
   }
 };
 
