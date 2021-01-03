ee2799d83aabca4ca95a935bb9bdc506f25521de
Donghwan Seo <tonky0110@gmail.com>
2021-01-03 15:42:00 +0900

    1.3 change state dispatch == 메신져 dispatch에 action(메시지)를 넣어 보면, store에 해당 하는 action을 찾아서 store를 변경하고 이를 저장. redux는 store를 한곳에 두고, 변경하는 function를 호출함으로써 전체 어플리케이션의 store를 관리한다.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const countModifier = (count = 0) => {
+const countModifier = (count = 0, action) => {
+  if (action.type === 'ADD') {
+    console.log("Call action.type = 'ADD'");
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    console.log("Call action.type = 'MINUS'");
+    return count - 1;
+  }
   return count;
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+
+console.log(countStore.getState());
+
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
