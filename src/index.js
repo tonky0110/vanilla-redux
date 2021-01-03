639c3568fe2e013d85a89ddcf763cf38d80cc094
Donghwan Seo <tonky0110@gmail.com>
2021-01-03 15:57:26 +0900

    1.3 redux subscribe() redux store가 변경되어지면(dispatch가 호출 되어지면) subscribe()를 등록하여 변경된 store의 값을 적용할 수 있도록 구독 등록할 수 있다.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+number.innerText = 0;
+
 const countModifier = (count = 0, action) => {
+  console.log(count, action);
   if (action.type === 'ADD') {
-    console.log("Call action.type = 'ADD'");
     return count + 1;
   } else if (action.type === 'MINUS') {
-    console.log("Call action.type = 'MINUS'");
     return count - 1;
   } else {
     return count;
@@ -18,16 +19,24 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-console.log(countStore.getState());
+// type 1 
+// add.addEventListener('click', () => countStore.dispatch({ type: "ADD" }));
+// minus.addEventListener('click', () => countStore.dispatch({ type: "MINUS" }));
 
+// type 2
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+}
 
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+}
 
-countStore.dispatch({ type: "MINUS" });
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
 
-console.log(countStore.getState());
+const onChange = () => {
+  console.log('there was a change on the store.');
+  number.innerText = countStore.getState();
+}
+countStore.subscribe(onChange);
